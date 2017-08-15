4f2f5d00631c5f99903b988b4c60aa30ceccf0e7
Mustafa Paksoy <paksoy@gmail.com>
2017-08-15 12:41:40 -0700

    Update site

--- new_post.sh
+++ new_post.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 NEW_FILE="_posts/`date +%Y-%m-%d`-$RANDOM.md"
-cp post-template.md "$NEW_FILE"
+cp _template/post-template.md "$NEW_FILE"
 sed -i .bak s/%DATE%/`date +%Y-%m-%dT%H:%M:%S%z`/ "$NEW_FILE"
 rm "$NEW_FILE.bak"
